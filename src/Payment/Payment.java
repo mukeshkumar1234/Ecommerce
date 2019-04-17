735b24ef0f502e64d03dfb8e0f67086558785acf
mukeshkumar1234 <mk4197762@gmail.com>
2019-04-17 09:43:48 +0530

    "This is my Payment"
    Change-Id: If9c6a1a646d0a7df91ee7fb038b952ff4fbc9fd6
    Signed-off-by: mukeshkumar1234 <mk4197762@gmail.com>

---
+++ src/Payment/Payment.java
@@ -0,0 +1,15 @@
+package Payment;
+
+public class Payment {
+
+	public Payment() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is payment Gatway");
+
+	}
+
+}
